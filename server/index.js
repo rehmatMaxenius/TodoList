d1fd3551464ec88ba766a8ed039ed712b35833a0
usama-maxenius <shehzadsofficial@gmail.com>
2021-09-24 00:43:06 +0500

    backend setup controller models and routes

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import listRoutes from './routes/listRoutes.js';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/lists', listRoutes);
+app.get('/', (req, res) => {
+    res.send('Welcome to todo list app');
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true  })
+.then(() => {
+    app.listen(PORT, () => console.log(`app running on port: ${PORT}`));
+})
+.catch(err => {
+    console.log(err.message);
+});
